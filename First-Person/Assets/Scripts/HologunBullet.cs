3608bafda019b6ac1ad2489bea99cb24a400fd4f
Humberto Benitez <benitezh@sheridancollege.ca>
2021-03-09 14:31:00 -0500

    Created hologun bullet prefab, which teleports player to the point of impact as long as the bullet hits the ground

---
+++ First-Person/Assets/Scripts/HologunBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologunBullet : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            Debug.Log(newPosition);
+            player.transform.position = newPosition;
+            Destroy(this.gameObject);
+        }
+    }
+}
