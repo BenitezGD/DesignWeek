79fca599b4ae44c6b14518a51c341e6c88f5613a
Humberto Benitez <benitezh@sheridancollege.ca>
2021-03-09 11:47:58 -0500

    Created repository with FIrst-Person controller and added pickup functionality

---
+++ First-Person/Assets/Scripts/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour
+{
+    public Score score;
+    public float rotate = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotate * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.SetActive(false);
+        score.addScore();
+    }
+}
