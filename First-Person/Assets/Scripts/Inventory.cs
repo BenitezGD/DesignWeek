a9b500cd25339736654676055a75ebb2379b3b9c
Humberto Benitez <benitezh@sheridancollege.ca>
2021-03-10 13:04:16 -0500

    Player can switch between Hologun and Sonic Emitter, adjusted mouse smoothing

---
+++ First-Person/Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject m471Hologun;
+    public GameObject m672SonicEmitter;
+
+    private void Start()
+    {
+        m471Hologun.gameObject.SetActive(true);
+        m672SonicEmitter.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1"))
+        {
+            m471Hologun.gameObject.SetActive(true);
+            m672SonicEmitter.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            m471Hologun.gameObject.SetActive(false);
+            m672SonicEmitter.gameObject.SetActive(true);
+        }
+    }
+}
