e421d1d7811662fc534d19f9ce61e224fb553cdf
Suraj Sirohi <surajsirohi1008@gmail.com>
2021-03-11 16:16:33 -0500

    Add Pre and Post Teleport Effect
    Create effects before and after teleportation

--- First-Person/Assets/Scripts/HologunBullet.cs
+++ First-Person/Assets/Scripts/HologunBullet.cs
@@ -12,6 +12,18 @@ public class HologunBullet : MonoBehaviour
     public float gravity;
     Vector3 newPosition;
 
+    [SerializeField]
+    GameObject teleportTargetEffect; //Particle effect created when ball hits the ground
+
+    [SerializeField]
+    float teleportTargetEffectGroundOffset; // How far above the ground will it be created
+
+    [SerializeField]
+    float teleportDelay; //Delay in seconds for teleportation after ball hits the ground
+
+    [SerializeField]
+    GameObject onTeleportEffect; //Create an effect when teleported to the target destination
+
 
     private void FixedUpdate()
     {
@@ -33,15 +45,36 @@ public class HologunBullet : MonoBehaviour
         if (other.gameObject.tag == "Ground")
         {
             newPosition = new Vector3(transform.position.x, transform.position.y + 0.6f, transform.position.z);
-            player.transform.position = newPosition;
+            //player.transform.position = newPosition;
+
+            StartCoroutine(TeleportCoroutine());
 
             gun.fired = false;
+
+            GameObject tempEffect = Instantiate(teleportTargetEffect, other.GetContact(0).point + other.GetContact(0).normal * teleportTargetEffectGroundOffset, teleportTargetEffect.transform.rotation);
+
+            Destroy(tempEffect, teleportDelay);
+
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<MeshRenderer>().enabled = false;
+
             Destroy(this.gameObject, 5);
         }
     }
 
     private void OnDestroy()
     {
-       // gun.fired = false;
+        // gun.fired = false;
+    }
+
+    IEnumerator TeleportCoroutine()
+    {
+        yield return new WaitForSeconds(teleportDelay);
+        player.transform.position = newPosition;
+
+        GameObject tempEffect = Instantiate(onTeleportEffect, transform.position, onTeleportEffect.transform.rotation);
+
+        Destroy(tempEffect, 0.6f);
     }
 }
