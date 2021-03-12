4518ca22fc37c83f221b9712230df769900026e6
Humberto Benitez <benitezh@sheridancollege.ca>
2021-03-12 05:57:22 -0500

    Update AudioManager.cs

--- First-Person/Assets/AudioManager.cs
+++ First-Person/Assets/AudioManager.cs
@@ -6,47 +6,21 @@ public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public AudioSource nightStalker;
-    public AudioSource LKM;
-    public AudioSource GME;
-    public AudioSource somethingSecret;
+    public Sound[] sounds;
 
-
-
-
-
-    private void OnTriggerEnter(Collider other)
+    void Awake()
     {
-        if(other.gameObject.name == "Trigger1")
+        foreach (Sound s in sounds)
         {
-            GME.Play();
-            LKM.Stop();
-            nightStalker.Stop();
-            somethingSecret.Stop();
-        }
-
-        if(other.gameObject.name == "Trigger2")
-        {
-            GME.Stop();
-            LKM.Play();
-            nightStalker.Stop();
-            somethingSecret.Stop();
-        }
-
-        if (other.gameObject.name == "Trigger3")
-        {
-            GME.Stop();
-            LKM.Stop();
-            nightStalker.Play();
-            somethingSecret.Stop();
-        }
-        
-        if (other.gameObject.name == "Trigger4")
-        {
-            GME.Stop();
-            LKM.Stop();
-            nightStalker.Stop();
-            somethingSecret.Play();
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
         }
     }
+
+    public void Play (string name)
+        {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        s.source.Play();
+        }
 }
