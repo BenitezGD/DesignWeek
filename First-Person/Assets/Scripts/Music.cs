f34e193312d58ef38f9d37c7ff5c7c5ea74ca155
Humberto Benitez <benitezh@sheridancollege.ca>
2021-03-12 06:40:00 -0500

    Fixed music bug

--- First-Person/Assets/Scripts/Music.cs
+++ First-Person/Assets/Scripts/Music.cs
@@ -12,41 +12,47 @@ public class Music : MonoBehaviour
     public AudioSource GME;
     public AudioSource somethingSecret;
 
+    public GameObject lastTrigger;
+
     public Image hl; 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Trigger1")
+        if (other.gameObject.name == "Trigger1" && lastTrigger.name != "Trigger1")
         {
             GME.Play();
             LKM.Stop();
             nightStalker.Stop();
             somethingSecret.Stop();
+            lastTrigger = other.gameObject;
         }
 
-        if (other.gameObject.name == "Trigger2")
+        if (other.gameObject.name == "Trigger2" && lastTrigger.name != "Trigger2")
         {
             GME.Stop();
             LKM.Play();
             nightStalker.Stop();
             somethingSecret.Stop();
+            lastTrigger = other.gameObject;
         }
 
-        if (other.gameObject.name == "Trigger3")
+        if (other.gameObject.name == "Trigger3" && lastTrigger.name != "Trigger3")
         {
             GME.Stop();
             LKM.Stop();
             nightStalker.Play();
             somethingSecret.Stop();
+            lastTrigger = other.gameObject;
         }
 
-        if (other.gameObject.name == "Trigger4")
+        if (other.gameObject.name == "Trigger4" && lastTrigger.name != "Trigger4")
         {
             GME.Stop();
             LKM.Stop();
             nightStalker.Stop();
             somethingSecret.Play();
             hl.gameObject.SetActive(true);
+            lastTrigger = other.gameObject;
         }
     }
 }
