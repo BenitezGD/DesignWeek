5b739c1cfb7e9c420009d1948b81e6df190e4fad
Humberto Benitez <benitezh@sheridancollege.ca>
2021-03-12 06:00:56 -0500

    Final commit hopefully

---
+++ First-Person/Assets/Scripts/Music.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioSource nightStalker;
+    public AudioSource LKM;
+    public AudioSource GME;
+    public AudioSource somethingSecret;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Trigger1")
+        {
+            GME.Play();
+            LKM.Stop();
+            nightStalker.Stop();
+            somethingSecret.Stop();
+        }
+
+        if (other.gameObject.name == "Trigger2")
+        {
+            GME.Stop();
+            LKM.Play();
+            nightStalker.Stop();
+            somethingSecret.Stop();
+        }
+
+        if (other.gameObject.name == "Trigger3")
+        {
+            GME.Stop();
+            LKM.Stop();
+            nightStalker.Play();
+            somethingSecret.Stop();
+        }
+
+        if (other.gameObject.name == "Trigger4")
+        {
+            GME.Stop();
+            LKM.Stop();
+            nightStalker.Stop();
+            somethingSecret.Play();
+        }
+    }
+}
